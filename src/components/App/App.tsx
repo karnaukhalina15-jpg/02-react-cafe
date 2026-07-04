d657b5e6b6d619fc032925e889ed1d2369859349
karnaukhalina15-jpg <karnaukh.alina15@gmail.com>
2026-07-05 01:35:50 +0200

    fix CafeInfo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import css from "./App.module.css";
 import type { Votes, VoteType } from "../../types/votes";
-import CafeInfo from "../CafeInfo/CofeInfo";
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import VoteOptions from "../VoteOptions/VoteOptions.tsx";
 import VoteStats from "../VoteStats/VoteStats.tsx";
 import Notification from "../Notification/Notification.tsx";
